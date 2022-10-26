ca98f2fe2c0f4571d6770e8b60b142dc2eda1c70
farhanchy99 <farhanchowdhury1999@gmail.com>
2022-10-26 13:54:15 +0600

    2nd commit

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
-const categories = require('./data/data.json')
+app.use(cors());
+
+const categories = require('./data/category.json')
+const data = require ('./data/data.json')
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+app.get('/courses/:id', (req, res) =>{
+    const id = req.params.id;
+    const categoryFullDetails = data.filter(
+        (n) => n.id === id
+    );
+    res.send(categoryFullDetails)
+}) 
 app.get('/card-categories', (req, res) =>{
     res.send(categories)
 }) 
